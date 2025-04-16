095e8c5b4f036c14ae3ff6734bd2e0a0fe1953a1
aisuvro <2842856+aisuvro@users.noreply.github.com>
2025-04-16 13:18:33 +0600

    Add nginx configuration for handling PHP requests and static files

--- docker-app-entrypoint.sh
+++ docker-app-entrypoint.sh
@@ -3,6 +3,15 @@
 # Set correct permissions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache
+composer install --no-dev --optimize-autoloader --no-interaction
+npm install --production --no-audit --no-fund
+php artisan key:generate --no-interaction
+php artisan opcache:clear --no-interaction
+php artisan config:cache --no-interaction
+php artisan route:cache --no-interaction
+php artisan view:cache --no-interaction
+php artisan storage:link --no-interaction
+php artisan migrate --force --no-interaction
 
 # Start PHP-FPM (this replaces the script process)
 exec php-fpm
